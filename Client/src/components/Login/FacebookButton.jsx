c05e45d33d901c03d8c0930d13b3065be5f92d27
Alanjoelc <alanjoelc09@gmail.com>
2021-08-02 19:47:55 -0300

    Login with Google&Facebook

--- Client/src/components/Login/FacebookButton.jsx
+++ Client/src/components/Login/FacebookButton.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import FacebookLogin from 'react-facebook-login';
 
 
-function facebook() {
+const FacebookButton = () => {
+    const responseFacebook = (response) => {
+        console.log(response)
+    }
+
+
     return (
-      <div className="App">
-      </div>
+    <div>
+        <FacebookLogin
+            appId={process.env.REACT_APP_ID_FACE}
+            autoLoad={true}
+            fields="name,email,picture"
+            callback={responseFacebook}
+            //cssClass='NOMBRE QUE QUIERAN' por si quieren darle un estilo propio le asignan una clase y le dan el diseño 
+            />
+    </div>
     );
-  }
-  
+}
 
-  export default facebook;
+export default FacebookButton;
---
+++ Client/src/components/Login/GoogleButton.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+
+
+const GoogleButton = () => {
+
+    const responseGoogle = (response) => {
+        console.log(response)
+    }
+
+
+    return (
+        <div>
+            <GoogleLogin
+                clientId={process.env.REACT_APP_ID_GOOGLE}
+                buttonText="Login"
+                // render = {renderProps => (
+                //     <button onClick={renderProps.onClick} disabled={renderProps.disabled}> por si quieren darle su propio estilo XD</button>
+                // )}
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
+        </div>
+    );
+}
+
+export default GoogleButton;
