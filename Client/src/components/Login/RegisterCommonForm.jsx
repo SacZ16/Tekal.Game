eb0529f41363269cbaa5e72f3f5d92a1f9bbc51c
Alanjoelc <alanjoelc09@gmail.com>
2021-08-03 11:59:05 -0300

    register common

---
+++ Client/src/components/Login/RegisterCommonForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+
+
+const RegisterCommonForm = () => {
+    return(
+        <div>
+            <Form>
+                <input type='text'  placeholder='...1'/>
+                <input type='text'  placeholder='...2'/>
+                <button> 😁 </button>
+            </Form>
+        </div>
+    )
+}
