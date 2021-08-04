ab76717a7849bafe6810f03c20d54b19367cb3a9
heroesh <fabio.quintero@gmail.com>
2021-08-03 19:05:11 -0500

    Up Template

--- Client/src/assets/videos.js
+++ Client/src/assets/videos.js
@@ -1,34 +1,46 @@
-import video1 from './videos/Video1.mp4'
-import video2 from './videos/Video2.mp4'
-import video3 from './videos/Video3.mov'
+import video1 from './videos/Video1.mp4';
+import video2 from './videos/Video2.mp4';
+import video3 from './videos/Video3.mov';
+import video4 from './videos/Video4.mp4';
+import video5 from './videos/Video5.mp4';
+import video6 from './videos/Video6.mp4';
+import video7 from './videos/Video7.mp4';
+
 
 const videos = [{
     id: 1,
-    name: video1
+    name: video1,
+    tipo: 'filed'
 },
 {
     id: 2,
-    name: video2
-},
-{
-    id: 1,
-    name: video1
+    name: video2,
+    tipo: 'filed'
 },
 {
     id: 3,
-    name: video3
+    name: video3,
+    tipo: 'watchers'
 },
 {
-    id: 3,
-    name: video3
+    id: 4,
+    name: video4,
+    tipo: 'filed'
 },
 {
-    id: 1,
-    name: video1
+    id: 5,
+    name: video5,
+    tipo: 'watchers'
 },
 {
-    id: 2,
-    name: video2
+    id: 6,
+    name: video6,
+    tipo: 'tarjet'
+},
+{
+    id: 7,
+    name: video7,
+    tipo: 'tarjet'
 }]
 
 export default videos
