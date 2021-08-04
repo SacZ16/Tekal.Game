02e2e8e301ac3c86265ab9c61cf9d0029614b25b
sofiapel <sofiacarlapellegrini@gmail.com>
2021-08-04 16:33:44 -0300

    probando

--- Database/App.js
+++ Database/App.js
@@ -1,10 +1,8 @@
-const { createUserTable,putUserInfoItems,getTable, putUserSessionsItems,querySession } = require("./Handlers/index")
+const { createUserTable,putUserInfoItems,getTable } = require("./Handlers/index")
 
 // createUserTable()
 //userId, name, lastname, email, password, age
 // putUserInfoItems("123", "Fabio","Pellegrini", "amapetrice@gmail.com","contraseña","43")
-//
-//putUserSessionsItems("124", ["fsdfs","dsfsdf"], ["1","0","0"])  
+    
 //tableName, keyValuePK, keyValueSK
-querySession("123")
-
+// getTable("User1", "123", "Info#123"); 
