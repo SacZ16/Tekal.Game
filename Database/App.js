926a7b740b1b594df8486fdfacba541e59c888a6
ivanox97 <arevaloivan1997@gmail.com>
2021-08-04 18:59:25 -0300

    handlers users and sessions

--- Database/App.js
+++ Database/App.js
@@ -1,8 +1,21 @@
-const { createUserTable,putUserInfoItems,getTable } = require("./Handlers/index")
+const { createUserTable, putUserInfoItems, putUserSessionItems, queryAllSessionsUser, userTablesQuery, getTable, deleteTable } = require("./Handlers/index")
 
 // createUserTable()
+
 //userId, name, lastname, email, password, age
-// putUserInfoItems("123", "Fabio","Pellegrini", "amapetrice@gmail.com","contraseña","43")
+// putUserInfoItems("Fabio", "Fabio","Pellegrini", "amapetrice@gmail.com","contraseña","43")
+
+// putUserInfoItems("Angel367676fgdfgfgt5e4tretre79", "Angel","Pellegrini", "amapetrice@gmail.com","contraseña","43")
     
 //tableName, keyValuePK, keyValueSK
-// getTable("User1", "123", "Info#123"); 
+// getTable("USER", "Ivanox", "INFO#Ivanox"); 
+
+// console.log(new Date().toString().replace(/ /g, "").slice(6,20))
+
+// //userId, presentations, answers
+putUserSessionItems("Fabio", ["dfdfdfdf", "chau"], [1,0])
+// queryAllSessionsUser("Fabio")
+
+// deleteTable("User1")
+
+// userTablesQuery("PK", "Angel367676fgdfgfgt5e4tretre79")
--- Database/Handlers/index.js
+++ Database/Handlers/index.js
@@ -9,35 +9,15 @@ const dynamodb = new AWS.DynamoDB();
 const  docClient = new AWS.DynamoDB.DocumentClient();
 
 const createUserTable = () => {
-    var params = {
-        TableName : "User1",
+    let params = {
+        TableName : "USER",
         KeySchema: [       
-            { AttributeName: "PK", KeyType: "HASH"},  //Partition key
-            {
-                AttributeName: "SK", 
-                KeyType: "RANGE"
-            }
-            // {
-            //     AttributeName: "UserSession", 
-            //     KeyType: "RANGE"
-            // }
+            { AttributeName: "PK", KeyType: "HASH"}, //Partition key
+            { AttributeName: "SK", KeyType: "RANGE"} //Sort Key
         ],
         AttributeDefinitions: [       
-            { 
-                AttributeName: "PK", 
-                AttributeType: "S" 
-            },
-            { 
-                AttributeName: "SK", 
-                AttributeType: "S" 
-            },
-            // { 
-            //     AttributeName: "UserSession", 
-            //     AttributeType: "S" 
-            // },
-
-
-
+            { AttributeName: "PK",  AttributeType: "S"},
+            { AttributeName: "SK", AttributeType: "S"},
         ],
         ProvisionedThroughput: {       
             ReadCapacityUnits: 10, 
@@ -55,54 +35,72 @@ const createUserTable = () => {
 }
 
 const putUserInfoItems = (userId, name, lastname, email, password, age) => {
-    const table = "User1";
-    const params = {
-        userId: userId,
-        name: name,
-        lastname: lastname,
-        email: email,
-        password: password,
-        age:age
+    const table = "USER";
+    const items = {
+        userId,
+        name,
+        lastname,
+        email,
+        password,
+        age
     }
-    var infoUser = `Info#${items.userId}`;
+    var infoUser = `INFO#${items.userId}`;
 
-    var params = {
+    let params = {
         TableName:table,
-        // Key:{
-        //     "PK": items.userId,
-        //     "SK": "qsy"
-        // },
-        // KeyConditionExpression: "#bbb=Info#:aaa",
-        // ExpressionAttributeNames: {
-        //     "#bbb":"SK"
-        // },
-        // ExpressionAttributeValues:{
-        //     ":aaa": items.UserId
-        // },
         Item:{
-            "PK": items.userId,
-            "SK": infoUser,
-            "name": items.name,
-            "lastName": items.lastname,
-            "email": items.email,
-            "password": items.password,
-            "age": items.age
+            "PK": {"S": items.userId},
+            "SK": {"S": infoUser},
+            "name": {"S": items.name},
+            "lastName":{"S": items.lastname},
+            "email": {"S": items.email},
+            "password": {"S": items.password},
+            "age": {"S": items.age}
         }
     };
-
-    console.log("Adding a new item...");
-    docClient.put(params, function(err, data) {
+    console.log("Adding a new user item...");
+    dynamodb.putItem(params, function(err, data) {
         if (err) {
             console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-        } else {
-            console.log("Added item:", JSON.stringify(data, null, 2));
+        } else { 
+            console.log("Added user item:", JSON.stringify(data, null, 2));
         }
     });
 }
 
-const getTable = (tableName, keyValuePK, keyValueSK) => {
+const putUserSessionItems = (userId, presentations, answers) => {
+    const table = "USER";
+    const sessionId = new Date().toString().replace(/ /g, "").slice(6,20);
+    const items = {
+        userId,
+        presentations, 
+        answers
+    }
+    const userSession = `SESSION#${items.userId}#${sessionId}`;
 
-    var params = {
+    let params = {
+        TableName:table,
+        Item:{
+            "PK": items.userId, 
+            "SK": userSession, 
+            "presentations": presentations,
+            "answers": answers
+        }
+    };
+
+    console.log("Adding a new Session...");
+    docClient.put(params, function(err, data) {
+        if (err) {
+            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Added Session:", JSON.stringify(data, null, 2));
+        }
+    });
+}
+
+
+const getTable = (tableName, keyValuePK, keyValueSK) => {
+    let params = {
         TableName: tableName,
         Key:{
             "PK" : keyValuePK,
@@ -115,27 +113,44 @@ const getTable = (tableName, keyValuePK, keyValueSK) => {
         if (err) {
             console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
         } else {
-            try{
-                const dataTable = await data;
-                console.log("GetItem succeeded:", dataTable);
-            }
-            catch{
-                console.log("caca");
-            }
-            
+            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
         }
     });
 }
 
-const tableQuery = (table, keyName, keyValue) => {
-    var params = {
-        TableName : table,
-        KeyConditionExpression: "#key = :keyValue",
+const queryAllSessionsUser = (userId) => {
+    let params = {
+        TableName : "USER",
+        KeyConditionExpression: "#PK = :PK AND #session > :session",
         ExpressionAttributeNames:{
-            "#key": keyName
+            "#PK": "PK",
+            "#session": "SK"
         },
         ExpressionAttributeValues: {
-            ":keyValue": keyValue
+            ":PK": userId,
+            ":session": `SESSION#${userId}`,
+        }
+    };
+
+    docClient.query(params, function(err, data) {
+        if (err) {
+            console.log("Unable to query. Error:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Query succeeded.");
+            console.log("Query description JSON:", JSON.stringify(data, null, 2));
+        }
+    });
+}
+
+const userTablesQuery = (keyName, keyValue) => {
+    let params = {
+        TableName : "USER",
+        KeyConditionExpression: "#PK = :PK",
+        ExpressionAttributeNames:{
+            "#PK": keyName
+        },
+        ExpressionAttributeValues: {
+            ":PK": keyValue
         }
     };
     
@@ -143,14 +158,14 @@ const tableQuery = (table, keyName, keyValue) => {
         if (err) {
             console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
         } else {
-            console.log("Query succeeded: " + JSON.stringify(data));
+            console.log("Query succeeded: " + JSON.stringify(data, null, 2));
         }
     });
     
 }
 
 const deleteTable = (tableName) => {
-    var params = {
+    let params = {
         TableName : tableName
     };
     
@@ -167,5 +182,9 @@ const deleteTable = (tableName) => {
 module.exports = {
     createUserTable,
     putUserInfoItems,
+    putUserSessionItems,
+    queryAllSessionsUser,
+    userTablesQuery,
     getTable,
+    deleteTable
 }
