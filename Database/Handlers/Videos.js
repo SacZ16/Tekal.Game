927c70c7be8d39265cda8914f965f86ca6075e5c
sofiapel <sofiacarlapellegrini@gmail.com>
2021-08-06 20:49:23 -0300

    function videos

--- Database/Handlers/Videos.js
+++ Database/Handlers/Videos.js
@@ -8,7 +8,7 @@ AWS.config.update({
 const dynamodb = new AWS.DynamoDB();
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-const TABLE_USER = "USER";
+//const TABLE_USER = "USER";
 
 //Crear videos 
 //put video => crear el pk (la url) y el campo vistas en 0 target en 0 anotaciones en 0 y watched by []
@@ -41,4 +41,101 @@ const createVideosTable = () => {
             console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
         }
     });
+}
+
+const putVideo = async (urlVideoId) => {
+    try{
+        let params = {
+            TableName: "VIDEO",
+            Item:{
+                "PK": { "S" :urlVideoId },
+                "views":  { "N" : "0"},
+                "wasTarget": { "N":"0" },
+                "targetHitted": { "N":"0" },
+                "watchedBy": {"SS" : [] }
+
+            }
+        };
+        
+        const userLogin = await docClient.put(params).promise();
+        console.log("Added user item");
+        return userLogin;
+    }
+    catch(error){
+        console.error("Unable to add item. Error JSON:", JSON.stringify(error, null, 2));
+    }
+}
+
+const putWasTarget = async (urlVideoId) => {
+    try {
+
+        let params = {
+            TableName: "VIDEO",
+            Key:{
+                "PK": urlVideoId,
+            },
+            UpdateExpression: "set wasTarget = :target",
+            ExpressionAttributeValues: {
+                ":target": wasTarget++
+            },
+        };
+
+        const hit = docClient.update(params).promise();
+        console.log("Added target:", JSON.stringify(hit, null, 2));
+        return hit;
+    }
+    catch(error){
+        console.error("Unable to hit. Error JSON:", JSON.stringify(error, null, 2));
+    }
+}
+
+//put watcher => agrega un elemento string al array
+const putWatcher = async (urlVideoId, userId) => {
+    try {
+
+        let params = {
+            TableName: "VIDEO",
+            Key:{
+                "PK": urlVideoId,
+            },
+            UpdateExpression: "SET #watchedBy = list_append(#watchedBy, :watcher)",
+            ExpressionAttributeNames : {
+                "#watchedBy" : "watchedBy"
+              },
+            ExpressionAttributeValues: {
+                ":watcher": [userId]
+            },
+        };
+
+        const watcher = docClient.update(params).promise();
+        console.log("Added user item:", JSON.stringify(watcher, null, 2));
+        return watcher;
+    }
+    catch(error){
+        console.error("Unable to add item. Error JSON:", JSON.stringify(error, null, 2));
+    }
+}
+
+//put anotaciones => agrega +1 al anotaciones
+const putHitted = async (urlVideoId) => {
+    try {
+
+        let params = {
+            TableName: "VIDEO",
+            Key:{
+                "PK": urlVideoId,
+            },
+            UpdateExpression: "set targetHitted = :hit",
+            ExpressionAttributeValues: {
+                ":hit": targetHitted++
+            },
+        };
+
+        const hit = docClient.update(params).promise();
+        console.log("Added hit:", JSON.stringify(hit, null, 2));
+        return hit;
+    }
+    catch(error){
+        console.error("Unable to hit. Error JSON:", JSON.stringify(error, null, 2));
+    }
 }
