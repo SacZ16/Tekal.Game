3ac02eab9308d7d20e29845437eb34922d6c04c9
Alanjoelc <alanjoelc09@gmail.com>
2021-08-05 22:19:05 -0300

    aaaaa

--- Client/src/components/Signin/RegisterEmail.jsx
+++ Client/src/components/Signin/RegisterEmail.jsx
@@ -13,35 +13,25 @@ const RegisterWithEmail = () => {
     const [confirmPass, setConfirmPass] = useState('');
 
     //Estos van a estar seteando errores (osea cuando los inputs se rellenen mal estos estados van a tener algo adentro)
-    const [errorEmail, setErrorEmail] = useState('');
-    const [errorConfirmEmail, setErrorConfirmEmail] = useState('');
-    const [errorPassword, setErrorPassword] = useState('');
-    const [errorConfirmPass, setErrorConfirmPass] = useState('');
 
-
-    const SendToBackEnd = () => {
-        //Esta funcion arranca cuando le dan al boton de registrarse 😎 limplia los estados de errores, y se vuelve a fijar si hay error en los inputs
-        setErrorEmail('')
-        setErrorConfirmEmail('')
-        setErrorPassword('')
-        setErrorConfirmPass('')
-        const emailReject = /^(([^<>()[].,;:\s@"]+(.[^<>()[].,;:\s@"]+)*)|(".+"))@(([^<>()[].,;:\s@"]+.)+[^<>()[].,;:\s@"]{2,})$/i;
+    const SendToBackEnd = (e) => {
+        e.preventDefault()
+        const emailReject = /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i
         if (!emailReject.test(email) && email.length > 0){
-            setErrorEmail(true);
-            console.log(emailReject)
+            console.log('ENTREEE')
             return;
         }
         if(email !== ConfirmEmail){
-            setErrorConfirmEmail('2');
+            console.log('ENTREEE222222222222')
             return;
         }
         const passwordReject = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
         if (!passwordReject.test(password) && password.length > 0){
-            setErrorPassword('3');
+            console.log('ENTREEE2222')
             return;
         }
         if(password !== confirmPass){
-            setErrorConfirmPass('4')
+            console.log('ENTasd')
             return;
         }
         const user = {
@@ -76,11 +66,11 @@ const RegisterWithEmail = () => {
             <p className='copyrightRegister'>© 2021 Tekal, Inc. All rights reserved</p>
             <form className='formRegister'>
             <img className='logoFormRegister' src={logoTekal} alt="Logo de Tekal"/>
-                <input className='inputFormRegister' placeholder='Email' name='email' type='text' onChange={(e) => setEmail(e.target.value)} required/>
+                <input className='inputFormRegister' placeholder='Email' name='email' type='text' onChange={(e) => setEmail(e.target.value)}/>
                     {/* <p>Invalid Email format</p> */}
-                <input className='inputFormRegister' placeholder='Confirm email' name='Cemail' type='text' onChange={(e) => setConfirmEmail(e.target.value)} required/>
+                <input className='inputFormRegister' placeholder='Confirm email' name='Cemail' type='text' onChange={(e) => setConfirmEmail(e.target.value)} />
                     {/* <p>Email must be the same</p> */}
-                <input className='inputFormRegister' placeholder='Password' name='password' onChange={(e) => setPassword(e.target.value)} type='password' required/>
+                <input className='inputFormRegister' placeholder='Password' name='password' onChange={(e) => setPassword(e.target.value)} type='password' />
                 {/*  <p>Minimum 8 characters</p>
                     <p>Maximum 15 characters</p>
                     <p>At least one capital number</p>
@@ -88,13 +78,13 @@ const RegisterWithEmail = () => {
                     <p>At least one lower case letter</p>
                     <p>No blanks</p>
                     <p>At least 1 special character</p> */}
-                <input className='inputFormRegister' placeholder='Confirm password' name='Cpassword' onChange={(e) => setConfirmPass(e.target.value)} type='password' required/>
+                <input className='inputFormRegister' placeholder='Confirm password' name='Cpassword' onChange={(e) => setConfirmPass(e.target.value)} type='password'/>
                     {/* <p>Passwords must be the same</p> */}
                 <p className='orRegister'><hr className='hr' width='40%' color='lightgrey'></hr>or<hr class='hr' width='40%' color='lightgrey'></hr></p>
                 <GoogleButton/>
                 <FacebookButton/>
+                <button className='buttonRegister' onClick={(e) => SendToBackEnd(e)}> Register </button>
             </form>
-                <button className='buttonRegister' onClick={SendToBackEnd}> Register </button>
         </>
     )
 }
