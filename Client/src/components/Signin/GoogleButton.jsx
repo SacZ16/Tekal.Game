05ba5cc08c20a2f89ae43385580fef083a48da02
SacZ16 <alexiscoronel545@gmail.com>
2021-08-08 15:05:57 -0300

    swicht

--- Client/src/components/Signin/GoogleButton.jsx
+++ Client/src/components/Signin/GoogleButton.jsx
@@ -7,16 +7,13 @@ const GoogleButton = () => {
     const responseGoogle = (response) => {
         SendDataGoogle(response.profileObj.email)
     }
-
-
-
     return (
         <div>
             <GoogleLogin
                 clientId={process.env.REACT_APP_ID_GOOGLE}
                 buttonText="Continue with Google"
                 render = {renderProps => (
-                    <button className='googleLogin' onClick={renderProps.onClick} disabled={renderProps.disabled}>
+                    <button style={{'border':'1px solid #1663A2'}} className='googleLogin' onClick={renderProps.onClick} disabled={renderProps.disabled}>
                     <p className='one'>Con</p>
                     <p className='two'>tin</p>
                     <p className='three'>ue w</p>
