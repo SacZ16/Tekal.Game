63e7963037a9cc10c8328fb75f313975d45a3587
sofiapel <sofiacarlapellegrini@gmail.com>
2021-08-19 00:26:51 -0300

    endpoint

--- Api/src/services/notViewedVideos.service.js
+++ Api/src/services/notViewedVideos.service.js
@@ -3,21 +3,24 @@ const { viewedVideos, queryAllAssets, queryPK } = require('../Controllers/dbFunc
 async function videosNotSeen(email) {
     try {
         const viewedVideos1 = await viewedVideos(email);
+        if(viewedVideos1){
+            const PKviewed = new Set(viewedVideos1.Items.map(v => v.PK));
+            const arrayViewed = Array.from(PKviewed);
+        }
 
-        const PKviewed = new Set(viewedVideos1.Items.map(v => v.PK));
-        const arrayViewed = Array.from(PKviewed);
 
         const allVideos = await queryAllAssets();
-        const setVideos = new Set(allVideos.Items.map(v => v.PK));
-        const arrayVideos = Array.from(setVideos);
+        console.log(allVideos)
+        // const setVideos = new Set(allVideos.Items.map(v => v.PK));
+        // const arrayVideos = Array.from(setVideos);
 
-        const notViewedVideos = arrayVideos.filter(n => !arrayViewed.includes(n));
-        const videos = notViewedVideos.map(async v => await queryPK(v))
-        return Promise.all(videos)
+        // const notViewedVideos = arrayVideos.filter(n => !arrayViewed.includes(n));
+        // const videos = notViewedVideos.map(async v => await queryPK(v))
+        // return Promise.all(videos)
     }catch (error) {
         console.log(error);
     }
 }
-
+videosNotSeen("amapetrice@gmail.com")
 
 module.exports = { videosNotSeen };
