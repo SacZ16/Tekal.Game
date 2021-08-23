6d83a155b50ea5495b6a5298d80d5738ee91aec9
ivanox97 <arevaloivan1997@gmail.com>
2021-08-23 20:08:04 -0300

    modularizar assetVideo

--- Api/src/routes/assetsVideoInfo.js
+++ Api/src/routes/assetsVideoInfo.js
@@ -1,41 +1,11 @@
 const { Router } = require('express');
 const router = Router();
-const { putAssets, updateView, queryAllInfoUser, updateAnnotationsCorrect } = require('../Controllers/dbFunctions');
-const {endpoint, endpointNoMemento} = require('../services/endpoint.service');
+const { loadEndpoints } = require('../services/assets.service');
 
 router.post('/', async (req, res) => {
     let info = req.body;
-    let email = info[0];
-    let urls = [];
     try {
-        const userInfo = await queryAllInfoUser(email);
-        const age = userInfo.Items[0].age;
-        const country = userInfo.Items[0].country;
-        for (let i = 2; i < info.length; i++) {
-            var object = info[i];
-            object.age = age;
-            object.mood = object.mood;
-            object.country = country;
-            object.pos = i - 2;
-            let pkAssetsTarget = info[i].type === "image"? endpointNoMemento(info[i].url) : endpoint(info[i].url);
-            object.url = pkAssetsTarget;
-            if (info[i].category === "target"){
-                updateView(pkAssetsTarget);
-            }
-            if (info[i].category === "target_repeat" && info[i].answer == 1){
-                updateAnnotationsCorrect(pkAssetsTarget);
-            }
-            if (urls.includes(pkAssetsTarget)) {
-                let index = urls.indexOf(pkAssetsTarget) + 1;
-                object.repeated = true;
-                object.pos_1st = object.pos - index;
-                object.lag = object.pos - object.pos_1st;
-            } else {
-                urls.push(pkAssetsTarget);
-                object.repeated = false;
-            }
-            await putAssets(email, object);
-        }
+        loadEndpoints(info);
         res.send("addedAsset");
     }
     catch (error) {
---
+++ Api/src/services/assets.service.js
@@ -0,0 +1,37 @@
+const { updateView, updateAnnotationsCorrect, putAssets, queryAllInfoUser } = require("../Controllers/dbFunctions");
+const { endpointNoMemento, endpoint } = require("./endpoint.service");
+
+function loadEndpoints(array){
+    let email = array[0];
+    let urls = [];
+    const userInfo = await queryAllInfoUser(email);
+    const age = userInfo.Items[0].age;
+    const country = userInfo.Items[0].country;
+    for (let i = 2; i < array.length; i++) {
+        var object = info[i];
+        object.age = age;
+        object.mood = object.mood;
+        object.country = country;
+        object.pos = i - 2;
+        let pkAssetsTarget = object.type === "image"? endpointNoMemento(object.url) : endpoint(object.url);
+        object.url = pkAssetsTarget;
+        if (array[i].category === "target"){
+            updateView(pkAssetsTarget);
+        }
+        if (array[i].category === "target_repeat" && array[i].answer == 1){
+            updateAnnotationsCorrect(pkAssetsTarget);
+        }
+        if (urls.includes(pkAssetsTarget)) {
+            let index = urls.indexOf(pkAssetsTarget) + 1;
+            object.repeated = true;
+            object.pos_1st = object.pos - index;
+            object.lag = object.pos - object.pos_1st;
+        } else {
+            urls.push(pkAssetsTarget);
+            object.repeated = false;
+        }
+        await putAssets(email, object);
+    }
+}
+
+module.exports = { loadEndpoints }
